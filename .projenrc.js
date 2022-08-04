d5de5c7b3a94ee7b32531d6acf345f3658c07dcf
Tianyi-W <tianyiwu@amazon.com>
2022-08-04 16:10:37 -0400

    chore: project created with projen

---
+++ .projenrc.js
@@ -0,0 +1,15 @@
+const { awscdk } = require('projen');
+const project = new awscdk.AwsCdkConstructLibrary({
+  author: 'Tianyi-W',
+  authorAddress: 'tianyiwu@amazon.com',
+  cdkVersion: '2.1.0',
+  defaultReleaseBranch: 'main',
+  name: 'awscdk-dynamodb-global-tables',
+  repositoryUrl: 'https://github.com/cdklabs/awscdk-dynamodb-global-tables.git',
+
+  // deps: [],                /* Runtime dependencies of this module. */
+  // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
+  // devDeps: [],             /* Build dependencies for this module. */
+  // packageName: undefined,  /* The "name" in package.json. */
+});
+project.synth();
