ff0e5d429340fe6526ed358bb4931f64af7258cd
Madeline Kusters <80541297+madeline-k@users.noreply.github.com>
2022-08-09 15:03:06 -0700

    Update projen configuration

--- .projenrc.js
+++ .projenrc.js
@@ -1,11 +1,13 @@
 const { awscdk } = require('projen');
 const project = new awscdk.AwsCdkConstructLibrary({
-  author: 'Tianyi-W',
-  authorAddress: 'tianyiwu@amazon.com',
+  author: 'Amazon Web Services',
+  description: 'L2 Construct Library for DynamoDB Global Tables',
   cdkVersion: '2.1.0',
   defaultReleaseBranch: 'main',
   name: 'awscdk-dynamodb-global-tables',
   repositoryUrl: 'https://github.com/cdklabs/awscdk-dynamodb-global-tables.git',
+  stability: 'experimental',
+  autoApproveUpgrades: true,
 
   // deps: [],                /* Runtime dependencies of this module. */
   // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
