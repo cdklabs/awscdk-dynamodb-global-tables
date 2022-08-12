c832ffd9f1c6d70b1f82816c4f2a5b1734b7a056
Tianyi-W <107507036+Tianyi-W@users.noreply.github.com>
2022-08-12 13:45:58 -0400

    Update global-table.ts
    implement grant(), grantReadData(), grantWriteData(), grantReadWriteData() in src/global-table.ts

--- src/global-table.ts
+++ src/global-table.ts
@@ -1,97 +1,92 @@
-import { App, Stack } from 'aws-cdk-lib';
-import * as assertions from 'aws-cdk-lib/assertions';
+import { IResource, RemovalPolicy, Resource, Stack } from 'aws-cdk-lib';
 import * as ddb from 'aws-cdk-lib/aws-dynamodb';
 import * as iam from 'aws-cdk-lib/aws-iam';
-import { GlobalTable } from '../src';
+import { Construct } from 'constructs';
+import * as perms from './perms';
 
-test('Create global table with partition key and default properties', () => {
-  const region = 'us-east-1';
-  const app = new App();
-  const newstack = new Stack(app, 'id', {
-    env: { region: region },
-  });
-  new GlobalTable(newstack, 'hello', {
-    partitionKey: {
-      name: 'id',
-      type: ddb.AttributeType.STRING,
-    },
-  });
-  const template = assertions.Template.fromStack(newstack);
-  template.resourceCountIs('AWS::DynamoDB::GlobalTable', 1);
-  template.hasResourceProperties('AWS::DynamoDB::GlobalTable', {
-    AttributeDefinitions: [{
-      AttributeName: 'id',
-      AttributeType: 'S',
-    }],
-    BillingMode: 'PAY_PER_REQUEST',
-    KeySchema: [{
-      AttributeName: 'id',
-      KeyType: 'HASH',
-    }],
-    Replicas: [{
-      Region: newstack.region,
-    }],
-  });
-});
+export interface IGlobalTable extends IResource {
+  grant(grantee: iam.IGrantable, ...actions: string[]): iam.Grant;
+  grantReadData(grantee: iam.IGrantable): iam.Grant;
+  grantWriteData(grantee: iam.IGrantable): iam.Grant;
+  grantReadWriteData(grantee: iam.IGrantable): iam.Grant;
+}
 
-test('"grant" allows adding arbitrary actions associated with this table resource (via testGrant)', () => {
-  testGrant(
-    ['action1', 'action2'], (p, t) => t.grant(p, 'dynamodb:action1', 'dynamodb:action2'));
-});
-
-test('"grantReadData" allows the principal to read data from the table', () => {
-  testGrant(
-    ['BatchGetItem', 'Query', 'GetItem', 'Scan', 'ConditionCheckItem', 'DescribeTable'], (p, t) => t.grantReadData(p));
-});
-
-test('"grantWriteData" allows the principal to write data to the table', () => {
-  testGrant(
-    ['PutItem', 'DescribeTable'], (p, t) => t.grantWriteData(p));
-});
-
-test('"grantReadWriteData" allows the principal to read/write data', () => {
-  testGrant([
-    'BatchGetItem', 'Query', 'GetItem', 'Scan',
-    'ConditionCheckItem', 'DescribeTable', 'PutItem',
-  ], (p, t) => t.grantReadWriteData(p));
-});
-
-function testGrant(expectedActions: string[], invocation: (user: iam.IPrincipal, table: GlobalTable) => void) {
-  // GIVEN
-  const app = new App();
-  const stack = new Stack(app);
-  const table = new GlobalTable(stack, 'my-table', {
-    partitionKey: {
-      name: 'id', type: ddb.AttributeType.STRING,
-    },
-  });
-  const user = new iam.User(stack, 'user');
-
-  // WHEN
-  invocation(user, table);
-
-  // THEN
-  const action = expectedActions.length > 1 ? expectedActions.map(a => `dynamodb:${a}`) : `dynamodb:${expectedActions[0]}`;
-  assertions.Template.fromStack(stack).hasResourceProperties('AWS::IAM::Policy', {
-    PolicyDocument: {
-      Statement: [
-        {
-          Action: action,
-          Effect: 'Allow',
-          Resource: {
-            'Fn::GetAtt': [
-              'mytable0324D45C',
-              'Arn',
-            ],
-          },
-        },
+abstract class GlobalTableBase extends Resource implements IGlobalTable {
+  public abstract readonly tableArn: string;
+  protected readonly regionalArns = new Array<string>();
+  public grant(grantee: iam.IGrantable, ...actions: string[]): iam.Grant {
+    return iam.Grant.addToPrincipal({
+      grantee,
+      actions,
+      resourceArns: [
+        this.tableArn,
       ],
-    },
-    PolicyName: 'userDefaultPolicy083DF682',
-    Users: [
+      scope: this,
+    });
+  }
+
+  public grantReadData(identity: iam.IGrantable): iam.Grant {
+    return iam.Grant.addToPrincipal({
+      grantee: identity,
+      actions: perms.READ_DATA_ACTIONS.concat(perms.DESCRIBE_TABLE), // ['s3:Get*']as many actions as you need
+      resourceArns: [this.tableArn],
+    });
+  }
+
+  public grantWriteData(grantee: iam.IGrantable): iam.Grant {
+    //const tableActions = perms.WRITE_DATA_ACTIONS.concat(perms.DESCRIBE_TABLE);
+    //const keyActions = perms.KEY_READ_ACTIONS.concat(perms.KEY_WRITE_ACTIONS);
+    //return this.combinedGrant(grantee, { keyActions, tableActions });
+    return iam.Grant.addToPrincipal({
+      grantee,
+      actions: perms.WRITE_DATA_ACTIONS.concat(perms.DESCRIBE_TABLE),
+      resourceArns: [this.tableArn],
+    });
+  }
+
+  public grantReadWriteData(grantee: iam.IGrantable): iam.Grant {
+    //const tableActions = perms.READ_DATA_ACTIONS.concat(perms.WRITE_DATA_ACTIONS).concat(perms.DESCRIBE_TABLE);
+    //const keyActions = perms.KEY_READ_ACTIONS.concat(perms.KEY_WRITE_ACTIONS);
+    //return this.combinedGrant(grantee, { keyActions, tableActions });
+    return iam.Grant.addToPrincipal({
+      grantee,
+      actions: perms.READ_DATA_ACTIONS.concat(perms.WRITE_DATA_ACTIONS).concat(perms.DESCRIBE_TABLE),
+      resourceArns: [this.tableArn],
+    });
+  }
+
+}
+
+export interface GlobalTableProps {
+  readonly partitionKey: ddb.Attribute;
+}
+
+export class GlobalTable extends GlobalTableBase {
+  public readonly tableArn: string;
+  constructor(scope: Construct, id: string, props: GlobalTableProps) {
+    super(scope, id);
+    const resource = new ddb.CfnGlobalTable(this, 'Resource', {
+      attributeDefinitions: [{
+        attributeName: props.partitionKey.name,
+        attributeType: props.partitionKey.type,
+      }],
+      billingMode: 'PAY_PER_REQUEST',
+      keySchema: [{
+        attributeName: props.partitionKey.name,
+        keyType: 'HASH',
+      }],
+      replicas: [{
+        region: Stack.of(scope).region,
+      }],
+    });
+    resource.applyRemovalPolicy(RemovalPolicy.RETAIN);
+    this.tableArn = this.getResourceArnAttribute(resource.attrArn,
+      //Stack.of(this).formatArn(exampleResourceArnComponents(Stack.of(scope).stackName)),
       {
-        Ref: 'user2C2B57AE',
+        service: 'dynamodb',
+        resource: 'GlobalTable',
+        resourceName: this.physicalName,
       },
-    ],
-  });
+    );
+  }
 }
